db4eb509c3165c2fe8f7a3871cf8c7178b4d121b
Jecket22 <hark.nothing@gmail.com>
2020-11-13 22:23:22 +0100

    commit

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const app = require("express")();
 const mongoose = require("mongoose");
 const port = 8080;
-const endpoints = require("./endpoints/index");
 
 mongoose.set("useFindAndModify", false);
 mongoose.set("useCreateIndex", true);
@@ -13,14 +12,23 @@ const dbOptions = {
 	poolSize: 5,
 	connectTimeoutMS: 10000,
 	family: 4
-};
+}
 
 mongoose.connect("mongodb://127.0.0.1:6969/gdnps", dbOptions);
 mongoose.connection.on("connected", () => {
-    console.log("[ALERT/mongoose] Mongoose connected at port 6969")
+	console.log("[ALERT/mongoose] Mongoose connected at port 6969")
 });
 mongoose.connection.on("error", err => {
-    console.error(err);
+	console.error(err);
 });
 
-app.listen(port, () => console.log(`[ALERT/express server] Server running on local port ${port}`))
+// yoinked from colon 
+app.run = {}
+directories.forEach(d => {
+  fs.readdirSync('./endpoints/' + d).forEach(x => {if (x.includes('.')) app.run[x.split('.')[0]] = require('./api/' + d + "/" + x) });
+});
+
+app.get("/database/accounts/registerGJAccount.php", function(req, res) { app.run.register(app, req, res) });
+app.get("*", function(res) { res.send("fuck") console.log("!") } );
+
+app.listen(port, () => console.log(`[ALERT/express server] Server running on local port ${port}`));
