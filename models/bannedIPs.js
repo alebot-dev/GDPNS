dc784796e2380fee9ef4ff5973518f2ab2dbc683
Jecket22 <hark.nothing@gmail.com>
2020-11-15 15:24:28 +0100

    Little improvement

--- models/bannedIPs.js
+++
@@ -1,7 +0,0 @@
-const { model, Schema } = require("mongoose");
-
-const bannedIPs = Schema({
-    IPs: {type: Array}
-});
-
-module.exports = model("BannedIPs", bannedIPs);
---
+++ models/bans.js
@@ -0,0 +1,8 @@
+const { model, Schema } = require("mongoose");
+
+const bans = Schema({
+	accounts: {type: Array}, // banned accounts
+    IPs: {type: Array} // banned IPs, if people try to bypass bans by making a new acc
+});
+
+module.exports = model("BannedIPs", bannedIPs);
