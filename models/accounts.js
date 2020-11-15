d89c5857a6c5a3d3cb55229f48d9f3670580b643
Jecket22 <hark.nothing@gmail.com>
2020-11-15 15:14:32 +0100

    removed useless stuff

--- models/accounts.js
+++ models/accounts.js
@@ -6,16 +6,10 @@ const accounts = Schema({
     username: {type: String, index: true, unique: true},
     password: {type: String},
     email: {type: String, unique: true},
-    saveData: {type: String},
-    isAdmin: {type: Boolean, default: false},
     friends: {type: Array},
     blocked: {type: Array},
-    blockedBy: {type: Array},
-    messages: {type: Number},
-    friendReqs: {type: Number},
-    cs: {type: Number},                                     // what the fuck is a cs
+    cs: {type: Number}, // Comment History, no known use afaik
     registerDate: {default: Date()},
-    friendsCount: {type: Number},
     youtube: {type: String},
     twitter: {type: String},
     twitch: {type: String},
