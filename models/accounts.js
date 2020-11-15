ce3172adb467d67c46cc54ce8f9058f780c8eb78
alebot-dev <qbotdev84@gmail.com>
2020-11-15 09:04:39 -0500

    Updated to have all necessary fields

--- models/accounts.js
+++ models/accounts.js
@@ -2,9 +2,21 @@ const { Schema, model } = require("mongoose");
 
 const accounts = Schema({
     accID: {type: Number, index: true, unique: true},
+    userID: {type: Number, index: true, unique: true},
     username: {type: String, index: true, unique: true},
     password: {type: String},
     email: {type: String, unique: true},
+    saveData: {type: String},
+    isAdmin: {type: Boolean, default: false},
+    friends: {type: Array},
+    blocked: {type: Array},
+    blockedBy: {type: Array},
+    messages: {type: Number},
+    friendReqs: {type: Number},
+    cs: {type: Number},                                     // what the fuck is a cs
+    salt: {type: String},
+    registerDate: {default: Date()},
+    friendsCount: {type: Number},
     youtube: {type: String},
     twitter: {type: String},
     twitch: {type: String},
