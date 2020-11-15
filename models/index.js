fd79c4260628a14cc9fef740adf6cca7cf9097c6
alebot-dev <qbotdev84@gmail.com>
2020-11-15 09:36:34 -0500

    Fix index.js

--- models/index.js
+++ models/index.js
@@ -1,12 +1,6 @@
 module.exports = {
-<<<<<<< HEAD
     AccountComments: require("./acomments"),
     Accounts: require("./accounts"),
-    BannedIPs: require("./bannedIPs"),
+    Bans: require("./bans"),
     Comment: require("./comment"),
-=======
-    accountComments: require("./acomments"),
-    accounts: require("./accounts"),
-    bans: require("./bans"),
->>>>>>> f76ccc2aefe810ffd40d86c96b7109375d42e621
 };
