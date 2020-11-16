dff30f6061d03ca822901743114a4f4ccf5669da
alebot-dev <qbotdev84@gmail.com>
2020-11-16 06:18:18 -0500

    Removed feature ID

--- models/dailyFeature.js
+++ models/dailyFeature.js
@@ -1,7 +1,6 @@
 const { Schema, model } = require("mongoose");
 
 const dailyFeature = Schema({
-    featureID: {type: Number, unique: true, index: true},
     levelID: {type: Number},
     timestamp: {default: new Date().getTime()},
     type: {type: Number, default: 0}
