ea332fb3558e771b666188746bf623a32295a61e
alebot-dev <qbotdev84@gmail.com>
2020-11-21 10:25:07 -0500

    asioufodishgfdsioughsioughsd the thingy for register works

--- endpoints/db_accounts.js
+++ endpoints/db_accounts.js
@@ -4,7 +4,23 @@ router.use(bodyParser.json());
 
 router.post("/registerGJAccount.php", async (req, res) => {
 	const { Account } = require("../models/index");
-	console.log(new Account);
+	const body = req.body;
+	try {
+		const Doc = await new Account({
+			accID: body.accID,
+			username: body.userName,
+			password: body.password,
+			email: body.email,
+			ms: body.ms,
+			frs: body.frs,
+			cs: body.cs,
+			ip: body.ip
+		});
+		const save = await Doc.save();
+		console.log(save);
+	} catch {
+		return res.send("-1").status(400);
+	}
 	res.send("1").status(200);
 });
 
